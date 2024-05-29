f2b9987a4c66368c8b716a4743e29b35bcc047c6
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-28 21:46:36 -0300

    feat:fogos
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- main.js
+++ main.js
@@ -2,51 +2,6 @@ var degree = 1800;
 var clicks = 0;
 
 $(document).ready(function(){
-
-	$('#spin').click(function(){
-		clicks++;
-
-		var newDegree = degree * clicks;
-		var extraDegree = Math.floor(Math.random()* (360 - 1 + 1)) +1;
-		totalDegree = newDegree + extraDegree;
-
-		$('#roleta .sec').each(function(){
-			var t = $(this);
-			var noY = 0;
-
-			var c = 0;
-			var n = 0;
-			var interval = setInterval(function(){
-				c++;
-				if(c === n){
-					clearInterval(interval);
-				}
-				var aoY = to.offset().top;
-				$('#txt').html(aoY);
-				console.log(aoY);
-
-				if(aoY < 23.83){
-					console.log('<<<');
-					$('#spin').addClass('spin');
-					setTimeout(function(){
-						$('#spin').removeClass('spin');
-
-					},100);
-				}
-			},10);
-
-			$('#interno-roleta').css({
-				'transform': 'rotate(' + totalDegree + 'deg)'
-			});
-			noY = t.offset().top;
-		});
-	});
-});
-
-var degree = 1800;
-var clicks = 0;
-
-$(document).ready(function(){
     $('#spin').click(function(){
         clicks++;
 
@@ -65,6 +20,11 @@ $(document).ready(function(){
                 spread: 70,
                 origin: { y: 0.6 }
             });
-        }, 6000); // Tempo de rotação da roleta (6s)
+
+            // Redirecionar após o efeito de confete
+            setTimeout(function(){
+                window.location.href = 'parabens.html';
+            }, 2000); // Tempo para o efeito de confete terminar (2 segundos)
+        }, 6000); // Tempo de rotação da roleta (6 segundos)
     });
 });
